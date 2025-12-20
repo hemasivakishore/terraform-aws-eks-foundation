8b425b1d6679ec14198b3f5abf5a94f6b1855fa4
hemasivakishore <hemasivakishore@gmail.com>
2025-12-20 23:28:28 +0530

    vpc and subnet(Private and public) added

--- variables.tf
+++ variables.tf
@@ -1,5 +1,22 @@
 variable "region" {
-    type = string
-    description = "AWS Region"
-    default = "us-east-1"
+  type        = string
+  description = "AWS Region"
+  default     = "us-east-1"
+}
+
+# Kubernetes VPC Variables
+
+variable "cidr_block" {
+  type    = string
+  default = "192.168.0.0/16"
+}
+
+variable "tags" {
+  type        = map(string)
+  description = "Tags to apply to all resources"
+  default = {
+    name      = "terraform-vpc"
+    terraform = "yes"
+    cluster   = "demo-eks-cluster"
+  }
 }
